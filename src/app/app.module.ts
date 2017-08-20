1ff439b3547f595641479612935cf8e10bf1539a
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2017-08-21 00:54:26 +0100

    Added bootstrap, normalize, typekit and ga analytics to site. Added blank hero component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
