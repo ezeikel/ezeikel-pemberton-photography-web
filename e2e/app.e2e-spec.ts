89c34725cb1370d7a1cbc9941daa135ca6359646
Angular CLI <angular-cli@angular.io>
2017-08-20 22:46:32 +0100

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { EzeikelPembertonPhotographyWebPage } from './app.po';
+
+describe('ezeikel-pemberton-photography-web App', () => {
+  let page: EzeikelPembertonPhotographyWebPage;
+
+  beforeEach(() => {
+    page = new EzeikelPembertonPhotographyWebPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
