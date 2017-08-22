5910804af1a900d5bae7fb67d343650cb457f0c6
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2017-08-22 01:32:11 +0100

    Added hero-image and hero-carousel components. Need to flesh these out.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './hero/hero.component';
+import { HeroImageComponent } from './hero-image/hero-image.component';
+import { HeroCarouselComponent } from './hero-carousel/hero-carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent
+    HeroImageComponent,
+    HeroCarouselComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hero-carousel/hero-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-carousel',
+  templateUrl: './hero-carousel.component.html',
+  styleUrls: ['./hero-carousel.component.css']
+})
+export class HeroCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
