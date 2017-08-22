251e549e7ff9988263f3f27dfe55c6927c4e6074
Ezeikel Pemberton <ezeikel_pemberton@discovery.com>
2017-08-22 18:50:08 +0100

    A bunch of stuff - routing, images, css based carousel, home component, site logo and some basical styling for home view.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { HeroImageComponent } from './hero-image/hero-image.component';
 import { HeroCarouselComponent } from './hero-carousel/hero-carousel.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: HomeComponent } //TODO: Replace with PageNotFoundComponent
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroImageComponent,
-    HeroCarouselComponent
+    HeroCarouselComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true }
+    )
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/hero-carousel/hero-carousel.component.ts
+++ src/app/hero-carousel/hero-carousel.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// Import the Image interface
+import { Image } from '../image/image.interface';
+
 @Component({
   selector: 'app-hero-carousel',
   templateUrl: './hero-carousel.component.html',
@@ -7,7 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroCarouselComponent implements OnInit {
 
-  constructor() { }
+  public images: Image[];
+
+  constructor() {
+    this.images = [
+      {'title': 'We are covered 1', 'url': './assets/images/slide-1.jpg'},
+      {'title': 'We are covered 2', 'url': './assets/images/slide-2.jpg'},
+      {'title': 'We are covered 3', 'url': './assets/images/slide-3.jpg'},
+      {'title': 'We are covered 4', 'url': './assets/images/slide-4.jpg'},
+      {'title': 'We are covered 5', 'url': './assets/images/slide-5.jpg'}
+    ];
+  }
 
   ngOnInit() {
   }
