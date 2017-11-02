cb52c328ec25d0426ab4c9daadb16fe0deb75a1c
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2017-11-02 22:28:41 +0000

    Now using sass for styling.

--- src/app/hero-header/hero-header.component.ts
+++ src/app/hero-header/hero-header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hero-header',
   templateUrl: './hero-header.component.html',
-  styleUrls: ['./hero-header.component.css']
+  styleUrls: ['./hero-header.component.scss']
 })
 export class HeroHeaderComponent implements OnInit {
 
