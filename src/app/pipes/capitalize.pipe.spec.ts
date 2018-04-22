34ec94ecf4bfe29b0219decc6415d43dbb2e217e
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-04-22 23:36:54 +0100

    Added test for pipe.

---
+++ src/app/pipes/capitalize.pipe.spec.ts
@@ -0,0 +1,13 @@
+import { CapitalizePipe } from './capitalize.pipe';
+
+describe('Capitalize', () => {
+  let pipe: CapitalizePipe;
+
+  beforeEach(() => {
+    pipe = new CapitalizePipe();
+  });
+
+  it('should capitalize the first letter of any string', () => {
+    expect(pipe.transform('ezeikel')).toBe('Ezeikel');
+  });
+});
--- src/app/pipes/capitalize.pipe.ts
+++ src/app/pipes/capitalize.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'capitalize' })
 export class CapitalizePipe implements PipeTransform {
-  transform(value: any) {
+  transform(value: string) {
     if (value) {
       return value.charAt(0).toUpperCase() + value.slice(1);
     }
