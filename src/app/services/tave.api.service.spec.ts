05fb3237f230c3470c0d49899f9aae107e06cc41
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-04-22 04:16:59 +0100

    Added spec file for api service.

---
+++ src/app/services/tave.api.service.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { TaveApiService } from './tave-api.service'
+
+describe('Service: LanguagesService', () => {
+  let service;
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule
+    ],
+    providers: [ 
+      TaveApiService 
+    ]
+  }));
+
+  beforeEach(inject([TaveApiService], s => {
+    service = s;
+  }));
+
+});
