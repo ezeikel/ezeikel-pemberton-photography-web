d24a4ce36c10ec27d36ab403dd62493f652d421c
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-07-28 20:29:45 +0100

    Added cta to details.

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -9,15 +9,18 @@ export class DetailsComponent implements OnInit {
   public items = [
     {
       copy: 'We are a team of photographers based in London, UK who specialise in documenting those moments that will live on in the memory for a lifetime.',
-      imageUrl: 'https://bit.ly/2AiGY3i'
+      imageUrl: 'https://bit.ly/2AiGY3i',
+      cta: false
     },
     {
       copy: 'We use every last drop of our creative juices to tell your story in the most unique way, while keeping as true as we can to the emotions and feelings at that time.',
-      imageUrl: 'https://bit.ly/2OogaBG'
+      imageUrl: 'https://bit.ly/2OogaBG',
+      cta: true
     },
     {
       copy: 'Our ultimate goal is to have our clients re-ignite those emotions and feelings everytime they look at our work.',
-      imageUrl: 'https://bit.ly/2uZKM4k'
+      imageUrl: 'https://bit.ly/2uZKM4k',
+      cta: false
     }
   ];
 
