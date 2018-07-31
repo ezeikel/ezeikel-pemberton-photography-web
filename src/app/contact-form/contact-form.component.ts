5446e268288fb976fcc4ce898f27b5ff4a3e7a07
Ezeikel Pemberton <ezeikel_pemberton@discovery.com>
2018-07-31 16:46:07 +0100

    Made jobTypes property public.

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { TaveApiService } from '../services/tave-api.service';
-import { ILead } from '../models/lead.interface';
+import { Component, OnInit } from "@angular/core";
+import { TaveApiService } from "../services/tave-api.service";
+import { ILead } from "../models/lead.interface";
 
 @Component({
-  selector: 'ep-photography-contact-form',
-  templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.scss']
+  selector: "ep-photography-contact-form",
+  templateUrl: "./contact-form.component.html",
+  styleUrls: ["./contact-form.component.scss"]
 })
 export class ContactFormComponent implements OnInit {
   public lead: ILead;
-  private _jobTypes = [
-    'Event',
-    'Pre Wedding Shoot',
-    'Proposal/Engagement',
-    'Traditional Engagement/Wedding',
-    'Wedding'
+  public JOBTYPES = [
+    "Event",
+    "Pre Wedding Shoot",
+    "Proposal/Engagement",
+    "Traditional Engagement/Wedding",
+    "Wedding"
   ];
   private _firstName: string;
   private _event: string;
@@ -24,16 +24,17 @@ export class ContactFormComponent implements OnInit {
 
   ngOnInit() {
     this.lead = {
-      SecretKey: '',
-      FirstName: '',
-      Email: '',
-      EmailConfirm: '',
-      JobType: '',
-      EventDate: ''
+      SecretKey: "",
+      FirstName: "",
+      Email: "",
+      EmailConfirm: "",
+      JobType: "",
+      EventDate: ""
     };
   }
 
-  uppercaseFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+  uppercaseFirstLetter = string =>
+    string.charAt(0).toUpperCase() + string.slice(1);
 
   onSubmit() {
     // Remove email confirm from data sent to api
@@ -42,10 +43,16 @@ export class ContactFormComponent implements OnInit {
     }
 
     // Clean up format of data being sent
-    this.lead.FirstName = this.uppercaseFirstLetter(this.lead.FirstName.trim().toLowerCase());
-    this.lead.LastName = this.uppercaseFirstLetter(this.lead.LastName.trim().toLowerCase());
+    this.lead.FirstName = this.uppercaseFirstLetter(
+      this.lead.FirstName.trim().toLowerCase()
+    );
+    this.lead.LastName = this.uppercaseFirstLetter(
+      this.lead.LastName.trim().toLowerCase()
+    );
     this.lead.Email = this.lead.Email.trim().toLowerCase();
-    this.lead.Message = this.uppercaseFirstLetter(this.lead.Message.trim().toLowerCase());
+    this.lead.Message = this.uppercaseFirstLetter(
+      this.lead.Message.trim().toLowerCase()
+    );
 
     // Send form data to tave api service
     this.taveApiService.createLead(this.lead);
@@ -54,7 +61,6 @@ export class ContactFormComponent implements OnInit {
     this._firstName = this.lead.FirstName;
     this._event = this.lead.JobType;
 
-    return this.submitted = true;
+    return (this.submitted = true);
   }
-
 }
