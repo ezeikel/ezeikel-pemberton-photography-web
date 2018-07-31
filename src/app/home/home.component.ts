69133faf76aa7936023cd689c3bd6435102a35f1
Ezeikel Pemberton <ezeikel_pemberton@discovery.com>
2018-07-31 16:41:48 +0100

    Check for undefined in route.fragment. Added husky to make sure precommit works.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,30 +1,27 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { skipWhile, takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { skipWhile, takeUntil } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 @Component({
-  selector: 'ep-photography-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "ep-photography-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit, OnDestroy {
   private _unsubscribe$ = new Subject();
 
   constructor(private _route: ActivatedRoute) {}
 
-  public ngOnInit() {
-
-  }
+  public ngOnInit() {}
 
   public ngAfterViewInit() {
     // listen to route fragment changes and scroll to position
-    this._route
-      .fragment
+    this._route.fragment
       .pipe(takeUntil(this._unsubscribe$))
-      .pipe(skipWhile(f => f === null))
+      .pipe(skipWhile(f => f === null || f === undefined))
       .subscribe(f => {
-        const element: any = document.querySelector(f ? `#${f}` : 'body');
+        const element: any = document.querySelector(f ? `#${f}` : "body");
 
         if (element) {
           this._scrollTo(element);
@@ -40,7 +37,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   private _scrollTo(element) {
     window.scrollTo({
       top: element.offsetTop,
-      behavior: 'smooth'
+      behavior: "smooth"
     });
   }
 }
