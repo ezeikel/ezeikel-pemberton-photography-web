4aedcb22a9e80041f2dd57188b00c6b8d1798c4a
Ezeikel Pemberton <ezeikel_pemberton@discovery.com>
2018-07-31 11:48:20 +0100

    Fixed get in touch button not scrolling to contact form.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,42 +1,60 @@
-import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { skipWhile, takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router, ActivatedRoute } from '@angular/router';
+import { skipWhile, takeUntil, switchMap } from 'rxjs/operators';
+import { Subject, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'ep-photography-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
-  private _unsubscribe = new Subject();
+export class HomeComponent implements OnInit, OnDestroy {
+  private _unsubscribe$ = new Subject();
 
-  constructor(private _route: ActivatedRoute) {}
+  constructor(private _location: Location, private _router: Router, private _route: ActivatedRoute) {}
 
   public ngOnInit() {
-  }
-
-  public ngAfterViewInit() {
+    // listen to route fragment changes and scroll to position
     this._route
       .fragment
-      .pipe(takeUntil(this._unsubscribe))
-      .pipe(skipWhile(val => val === null))
-      .subscribe(val => {
-        if (val) {
-          const el = document.querySelector(`#${val}`);
-          const pos = el.getBoundingClientRect().top;
+      .pipe(takeUntil(this._unsubscribe$))
+      .pipe(skipWhile(f => f === null))
+      .subscribe(f => {
+        const element: any = document.querySelector(f ? `#${f}` : 'body');
 
+        if (element) {
           window.scrollTo({
-            top: pos,
+            top: element.offsetTop,
             behavior: 'smooth'
           });
         }
+
       });
+
+    // TODO: This not working correctly
+    // clear fragment from url
+
+    // fromEvent(window, 'scroll')
+    //   .pipe(takeUntil(this._unsubscribe$))
+    //   .pipe(switchMap(() => this._route.fragment))
+    //   .pipe(takeUntil(this._unsubscribe$))
+    //   .pipe(skipWhile(f => f === null))
+    //   .subscribe(f => {
+    //     const element: any = document.querySelector(f ? `#${f}` : 'body');
+    //     const offsetTop = element.offsetTop;
+
+    //     if (offsetTop !== window.scrollY) {
+    //       const pathWithoutHash =  this._location.path(false);
+    //       this._location.replaceState(pathWithoutHash)
+    //     }
+    //   });
+
   }
 
   public ngOnDestroy() {
-    this._unsubscribe.next();
-    this._unsubscribe.complete();
+    this._unsubscribe$.next();
+    this._unsubscribe$.complete();
   }
 
 }
