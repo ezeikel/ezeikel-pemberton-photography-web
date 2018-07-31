1812714a2a4c9cb6e3cc576746bee0b5b5eab4c1
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-07-31 23:10:36 +0100

    Using svg with different fills instead of two pngs for logo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireStorageModule } from "angularfire2/storage";
 
-import { environment } from './../environments/environment';
-import { TaveApiService } from './services/tave-api.service'
-import { FirebaseService } from './services/firebase.service';
-import { UiService } from './services/ui.service';
-import { EqualValidatorDirective } from './directives/equals-validator.directive';
-import { CapitalizePipe } from './pipes/capitalize.pipe';
+import { environment } from "./../environments/environment";
+import { TaveApiService } from "./services/tave-api.service";
+import { FirebaseService } from "./services/firebase.service";
+import { UiService } from "./services/ui.service";
+import { EqualValidatorDirective } from "./directives/equals-validator.directive";
+import { CapitalizePipe } from "./pipes/capitalize.pipe";
 
-import { AppComponent } from './app.component';
-import { HeroCarouselComponent } from './hero-carousel/hero-carousel.component';
-import { HomeComponent } from './home/home.component';
-import { BlogPreviewComponent } from './blog-preview/blog-preview.component';
-import { ScrollDownComponent } from './scroll-down/scroll-down.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
-import { BlogComponent } from './blog/blog.component';
-import { BlogPostComponent } from './blog-post/blog-post.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { HamburgerComponent } from './hamburger/hamburger.component';
-import { DetailsComponent } from './details/details.component';
-import { ContactComponent } from './contact/contact.component';
+import { AppComponent } from "./app.component";
+import { HeroCarouselComponent } from "./hero-carousel/hero-carousel.component";
+import { HomeComponent } from "./home/home.component";
+import { BlogPreviewComponent } from "./blog-preview/blog-preview.component";
+import { ScrollDownComponent } from "./scroll-down/scroll-down.component";
+import { ContactFormComponent } from "./contact-form/contact-form.component";
+import { BlogComponent } from "./blog/blog.component";
+import { BlogPostComponent } from "./blog-post/blog-post.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { AppRoutingModule } from ".//app-routing.module";
+import { HamburgerComponent } from "./hamburger/hamburger.component";
+import { DetailsComponent } from "./details/details.component";
+import { ContactComponent } from "./contact/contact.component";
+import { LogoComponent } from "./logo/logo.component";
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import { ContactComponent } from './contact/contact.component';
     HeaderComponent,
     HamburgerComponent,
     DetailsComponent,
-    ContactComponent
+    ContactComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
@@ -56,11 +58,7 @@ import { ContactComponent } from './contact/contact.component';
     AngularFireDatabaseModule, // imports firebase/database, only needed for database features
     AngularFireStorageModule // imports firebase/storage only needed for storage features
   ],
-  providers: [
-    TaveApiService,
-    FirebaseService,
-    UiService
-  ],
-  bootstrap: [ AppComponent ]
+  providers: [TaveApiService, FirebaseService, UiService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
