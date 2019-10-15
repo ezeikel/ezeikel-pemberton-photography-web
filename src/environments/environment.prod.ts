1f68bd30b832bb0068741c79a1ec2721f290e32b
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2019-10-15 21:20:55 +0100

    Trying to get Universal to work in Now.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,5 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: `AIzaSyBLBU1eVuVZdoCBZLi2HSOhDzGtYG5jlMQ`,
-    authDomain: `ezeikel-pemberton-photography.firebaseapp.com`,
-    databaseURL: `https://ezeikel-pemberton-photography.firebaseio.com`,
-    projectId: `ezeikel-pemberton-photography`,
-    storageBucket: `ezeikel-pemberton-photography.appspot.com`,
-    messagingSenderId: `550055016794`,
-  },
   contentful: {
     spaceId: `gjba64va93uk`,
     token: `oV8V1p_z8ycmTbYnnd47JfSAKzAlLc4pJ4-R4psRpOk`,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: `AIzaSyBLBU1eVuVZdoCBZLi2HSOhDzGtYG5jlMQ`,
-    authDomain: `ezeikel-pemberton-photography.firebaseapp.com`,
-    databaseURL: `https://ezeikel-pemberton-photography.firebaseio.com`,
-    projectId: `ezeikel-pemberton-photography`,
-    storageBucket: `ezeikel-pemberton-photography.appspot.com`,
-    messagingSenderId: `550055016794`,
-  },
   contentful: {
     spaceId: `gjba64va93uk`,
     token: `oV8V1p_z8ycmTbYnnd47JfSAKzAlLc4pJ4-R4psRpOk`,
